fc08e517e2accc177fd8e133b14a92d89018e386
Харитонюк Алёна Васильевна <a.kharitonyuk@ase-ec.ru>
2025-06-20 16:21:04 +0300

    2724. Sort By

---
+++ SortBy/ex3.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+  return arr.sort((a, b) => fn(a) - fn(b));
+    
+};
+
+arr = [[3, 4], [5, 2], [10, 1]], fn = (x) => x[1]
+
+console.log(sortBy(arr, fn));
